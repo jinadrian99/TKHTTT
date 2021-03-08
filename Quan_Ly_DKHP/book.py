481d93ae5c1b1ce225ad6b556e9183d0be40bde9
hanquocadrian <hanquocadrian@gmail.com>
2021-03-08 10:11:15 +0700

    Sort Folder

--- Quan_Ly_DKHP/book.py
+++ Quan_Ly_DKHP/book.py
@@ -6,7 +6,9 @@ class book (osv.osv):
     _columns = {
         'hocphan':fields.char('Học phần', size=25, required=True, translate=True),
         'sotinchi':fields.integer('Số tín chỉ'),
-        'hptienquyet':fields.integer('Học phần tiên quyết')
+        'hptienquyet':fields.integer('Học phần tiên quyết'),
+        'tbl_hptq': fields.many2many('hp.hp', 'hp_hp', 'id', 'create_uid', ''),
+        # 'project_ids': fields.many2many('project.project', 'project_task_type_rel', 'type_id', 'project_id', 'Projects'),
     }
     
 book()
